a3222b4a2032c4750c9c45130360ab8dd1684cb6
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2021-04-11 03:39:15 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace betterThanYou {
+	partial class Form1 {
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.richTextBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14);
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(1600, 600);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.ClientSize = new System.Drawing.Size(1600, 600);
+			int h = this.ClientSize.Height;
+			int w = this.ClientSize.Width;
+			this.MaximumSize = new System.Drawing.Size(w, h);
+			this.MinimumSize = new System.Drawing.Size(w, h);
+			this.Controls.Add(this.richTextBox1);
+			this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		
+		private System.Windows.Forms.RichTextBox richTextBox1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace betterThanYou {
+	public partial class Form1 : Form {
+		Dictionary<int, Process> processDict = new Dictionary<int, Process>();
+		List<(int, byte, string, DateTime, bool)> log = new List<(int, byte, string, DateTime, bool)>();
+
+		public void UpdateProcessList(Object myObject, EventArgs myEventArgs) {
+			Process[] processArray = Process.GetProcesses();
+			Dictionary<int, Process> newProcessDict = new Dictionary<int, Process>();
+
+			for (int i = 0; i < processArray.Length; i++)
+				newProcessDict[processArray[i].Id] = processArray[i];
+
+			foreach (KeyValuePair<int, Process> entry in newProcessDict) {
+				if (!processDict.ContainsKey(entry.Key)) {
+					processDict[entry.Key] = entry.Value;
+					log.Add((entry.Key, 1, entry.Value.ProcessName, DateTime.Now, true));
+				}
+			}
+
+			List<int> toBeRemoved = new List<int>();
+			foreach (KeyValuePair<int, Process> entry in processDict) {
+				if (!newProcessDict.ContainsKey(entry.Key)) {
+					toBeRemoved.Add(entry.Key);
+					log.Add((entry.Value.Id, 0, entry.Value.ProcessName, DateTime.Now, true));
+				}
+			}
+
+			foreach (int i in toBeRemoved)
+				processDict.Remove(i);
+
+			newProcessDict = null;
+			toBeRemoved = null;
+			this.UpdateTextBox();
+		}
+
+		public void AppendText(RichTextBox box, string text, Color color) {
+			box.SelectionStart = box.TextLength;
+			box.SelectionLength = 0;
+
+			box.SelectionColor = color;
+			box.AppendText(text);
+			box.SelectionColor = box.ForeColor;
+		}
+
+		public string getPaddedString((int, byte, string, DateTime, bool) item) {
+			StringBuilder builder = new StringBuilder();
+			builder.Append(item.Item1);
+			while (builder.Length < 12)
+				builder.Append(" ");
+
+			builder.Append(item.Item3);
+			while (builder.Length < 12 + 64)
+				builder.Append(" ");
+
+			builder.Append(item.Item4.ToString());
+			while (builder.Length < 12 + 64 + 20)
+				builder.Append(" ");
+
+			builder.Append("\n");
+			return builder.ToString();
+		}
+
+		public void UpdateTextBox() {
+			//for (int i = log.Count - 1; i >= 0; i --) {
+			for (int i = 0; i < log.Count; i++) {
+				if (log[i].Item5) {
+					log[i] = (log[i].Item1, log[i].Item2, log[i].Item3, log[i].Item4, false);
+					if (log[i].Item2 == 1) {
+						//this.richTextBox1.AppendText(getPaddedString(log[i]));
+						AppendText(this.richTextBox1, getPaddedString(log[i]), Color.LightBlue);
+					} else {
+						AppendText(this.richTextBox1, getPaddedString(log[i]), Color.IndianRed);
+					}
+				}
+			}
+		}
+
+		public Form1() {
+			InitializeComponent();
+			Timer timer = new Timer();
+			timer.Interval = 2000;
+			timer.Tick += new EventHandler(UpdateProcessList);
+			timer.Enabled = true;
+		}
+	}
+}
